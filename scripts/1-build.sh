7841107f11a7019be1b06701c6f501b9e54936f5
Luke Gessler <ldg3fa@virginia.edu>
2015-12-09 20:50:24 -0500

    added startup scripts

---
+++ scripts/1-build.sh
@@ -0,0 +1,8 @@
+screen -d -m target/release/corpex-index build sharded/hindmonocorp05.plaintext.sorted.shard0 sharded-sets/set0.fst
+screen -d -m target/release/corpex-index build sharded/hindmonocorp05.plaintext.sorted.shard1 sharded-sets/set1.fst
+screen -d -m target/release/corpex-index build sharded/hindmonocorp05.plaintext.sorted.shard2 sharded-sets/set2.fst
+screen -d -m target/release/corpex-index build sharded/hindmonocorp05.plaintext.sorted.shard3 sharded-sets/set3.fst
+screen -d -m target/release/corpex-index build sharded/hindmonocorp05.plaintext.sorted.shard4 sharded-sets/set4.fst
+screen -d -m target/release/corpex-index build sharded/hindmonocorp05.plaintext.sorted.shard5 sharded-sets/set5.fst
+screen -d -m target/release/corpex-index build sharded/hindmonocorp05.plaintext.sorted.shard6 sharded-sets/set6.fst
+screen -d -m target/release/corpex-index build sharded/hindmonocorp05.plaintext.sorted.shard7 sharded-sets/set7.fst
